df268dc8cb1cf5be856dd5e6447f7577e05524bd
shooontan <logo.bee.doo19@gmail.com>
2019-06-16 11:13:25 +0900

    add virtual scroll

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
-    <template v-if="ids.length > 0">
-      <div v-for="(id, index) in ids" :key="index">
+  <div class="container" :style="containerStyle">
+    <template v-if="displayIds.length > 0">
+      <div v-for="(id, index) in displayIds" :key="index">
         <img
           :src="item(id)[1] ? item(id)[1].url : item(id)[0].url"
           class="image"
@@ -11,20 +11,19 @@
     <template v-else>
       ...
     </template>
-    <template v-if="loading">
-      <div v-for="index in 10" :key="`loaing-${index}`">
-        <div class="loaing"></div>
-      </div>
-    </template>
-    <div class="next-man">
-      <button @click="get">next</button>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 import { Items, Image } from '@/types/response'
+
+const { ceil, floor, min } = Math
+
+const HEIGHT = 200
+const SHOW_MAX_ITEM_COUNT = 30
+const MAX_COL = 7
+const MARGIN_ROW = 1
 
 @Component({})
 export default class IndexPage extends Vue {
@@ -33,9 +32,21 @@ export default class IndexPage extends Vue {
   loading: boolean = false
   observer: IntersectionObserver | undefined = undefined
 
+  position = {
+    x: 0,
+    y: 0
+  }
+
+  offset = 0
+  col = 7
+
   async created() {
-    await this.get()
-    this.createObserver()
+    if (process.client) {
+      this.resize()
+      await this.get()
+      addEventListener('scroll', this.scroll)
+      addEventListener('resize', this.resize)
+    }
   }
 
   async get() {
@@ -63,26 +74,64 @@ export default class IndexPage extends Vue {
     }
   }
 
-  createObserver() {
-    if (typeof IntersectionObserver !== 'function') {
-      return
-    }
-    this.observer = new IntersectionObserver(
-      async () => {
-        await this.get()
-      },
-      {
-        threshold: 0
-      }
-    )
+  get displayIds() {
+    const ids: string[] = []
+    const { col, offset } = this
 
-    const target = document.querySelector('.next-man')
-    this.observer.observe(target as Element)
+    for (
+      let index = offset;
+      index < offset + SHOW_MAX_ITEM_COUNT + MARGIN_ROW * col;
+      index++
+    ) {
+      if (this.ids[index]) {
+        ids.push(this.ids[index])
+      }
+    }
+
+    return ids
+  }
+
+  get containerStyle() {
+    return {
+      'padding-top': `${this.paddingTop}px`,
+      'padding-bottom': `${this.paddingBottom}px`
+    }
+  }
+
+  get paddingTop() {
+    return (this.offset * HEIGHT) / this.col
+  }
+
+  get paddingBottom() {
+    const { col } = this
+    return (
+      (ceil(this.ids.length / col) -
+        ceil(this.offset / col) -
+        ceil(this.displayIds.length / col)) *
+      HEIGHT
+    )
   }
 
   item(id) {
     const item = this.images.get(id)
     return item
+  }
+
+  scroll() {
+    this.position.y = window.pageYOffset
+    this.offset = floor(this.position.y / HEIGHT) * this.col
+  }
+
+  resize() {
+    this.position.x = window.innerWidth
+    this.col = min(floor(this.position.x / HEIGHT), MAX_COL)
+  }
+
+  @Watch('offset')
+  onchangeOffset() {
+    if (this.paddingBottom / HEIGHT < 3) {
+      this.get()
+    }
   }
 }
 </script>
@@ -91,6 +140,7 @@ export default class IndexPage extends Vue {
 .container {
   display: flex;
   flex-wrap: wrap;
+  max-width: 1400px; /* 200*7 */
 }
 
 .image {
